8c9e1d1fac27d678a8b2d99c47a163558b7fac9c
os <osss151095@gmail>
2023-05-03 16:14:33 +0300

    сделал поддержку темной темы

--- app/src/main/java/com/example/newsfeatcherdz/App.kt
+++ app/src/main/java/com/example/newsfeatcherdz/App.kt
@@ -1,7 +1,8 @@
 package com.example.newsfeatcherdz
 
 import android.app.Application
-import com.example.newsfeatcherdz.feature.article_details_screen.di.articleDetailsModule
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.newsfeatcherdz.feature.articledetailsscreen.di.articleDetailsModule
 import com.example.newsfeatcherdz.feature.bookmarks.di.bookmarksModule
 import com.example.newsfeatcherdz.feature.di.mainScreenModule
 import org.koin.android.ext.koin.androidContext
@@ -18,6 +19,9 @@ class App: Application() {
                 androidContext(this@App)
                 modules(networkModule, mainScreenModule, bookmarksModule, databaseModule,articleDetailsModule)
             }
+
+        // чтобы приложение использовало темную тему в зависимости от системы
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
 
     }
 }
--- app/src/main/java/com/example/newsfeatcherdz/feature/article_details_screen/ui/Contract.kt
+++
@@ -1,8 +0,0 @@
-package com.example.newsfeatcherdz.feature.article_details_screen.ui
-
-data class ViewState(
-    val title: String,
-    val description: String,
-    val urlImage: String
-)
-
